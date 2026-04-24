4099fe7e73ac1f275a03a158e370269682abeb36
Nahum Trinidad Venancio <nahum.trinidadv@gmail.com>
2026-04-24 17:55:18 -0500

    feat: use println to show the user profile

---
+++ src/MiPerfil.java
@@ -0,0 +1,22 @@
+public class MiPerfil {
+
+    public static void main(String[] args){
+        String name = "Nahum";
+        int age = 39;
+        double average = 15.5;
+        int totalCourses = 5;
+         boolean isAcademicWorkloadHihg = totalCourses >= 5;
+
+        System.out.println("############################");
+        System.out.println("   MI PERFIL DE ESTUDIANTE");
+        System.out.println("############################");
+        System.out.println("Nomre      :" + name);
+        System.out.println("Edad       :" + age);
+        System.out.println("Promedio   :" + average + "/20");
+        System.out.println("Carga      :" +  "M[as de 5 cursos: " + isAcademicWorkloadHihg);
+
+    }
+
+
+    
+}
