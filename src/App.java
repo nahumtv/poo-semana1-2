153be354ebbbd5a5984fc858955a8c0607c4ad4e
Nahum Trinidad Venancio <nahum.trinidadv@gmail.com>
2026-04-24 17:51:04 -0500

    feat: add App.java file

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+        // Tipos de datos
+        int  edad = 39;
+        double sueldo  = 1000.50;
+        boolean casado = true;
+        char sexo = 'M';
+        String nombreCompleto = "Nahum Trinidad";
+
+        System.out.println("Datos del Trabajador");
+        System.out.println("Nombre y edad " + nombreCompleto + " tiene " + edad + " años");
+        System.out.println("es del sexo " +  sexo + " casado: " + casado +" y tiene un sueldo de " + sueldo);
+   }
+   
+}
